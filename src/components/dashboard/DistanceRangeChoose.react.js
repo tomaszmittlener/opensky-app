25d9b765e38b66285413171c1ae820940e161cb9
Tomasz Mittlener <t.mittlener@gmail.com>
2017-11-09 17:05:40 +0100

    feat: style distance choose buttons
    feat: style distance choose buttons

--- src/components/dashboard/DistanceRangeChoose.react.js
+++ src/components/dashboard/DistanceRangeChoose.react.js
@@ -5,6 +5,13 @@ import ButtonGroup from 'react-bootstrap/lib/ButtonGroup'
 import Button from 'react-bootstrap/lib/Button'
 import DropdownButton from 'react-bootstrap/lib/DropdownButton'
 import MenuItem from 'react-bootstrap/lib/MenuItem'
+import styled from 'styled-components'
+
+const ButtonsGroupContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  padding: 10px 15px 0 0;
+`
 
 class DistanceRangeChoose extends React.Component {
   onSetDistanceClick(number) {
@@ -14,18 +21,20 @@ class DistanceRangeChoose extends React.Component {
 
   render() {
     return (
-      <ButtonGroup>
-        <Button onClick={() => this.onSetDistanceClick(10)}>10</Button>
-        <Button onClick={() => this.onSetDistanceClick(50)}>50</Button>
-        <DropdownButton title="Distance" id="bg-nested-dropdown">
-          <MenuItem eventKey="1" onClick={() => this.onSetDistanceClick(100)}>
-            100
-          </MenuItem>
-          <MenuItem eventKey="2" onClick={() => this.onSetDistanceClick(1000)}>
-            1000
-          </MenuItem>
-        </DropdownButton>
-      </ButtonGroup>
+      <ButtonsGroupContainer>
+        <ButtonGroup>
+          <Button onClick={() => this.onSetDistanceClick(10)}>10</Button>
+          <Button onClick={() => this.onSetDistanceClick(50)}>50</Button>
+          <DropdownButton title="Distance" id="bg-nested-dropdown">
+            <MenuItem eventKey="1" onClick={() => this.onSetDistanceClick(100)}>
+              100
+            </MenuItem>
+            <MenuItem eventKey="2" onClick={() => this.onSetDistanceClick(1000)}>
+              1000
+            </MenuItem>
+          </DropdownButton>
+        </ButtonGroup>
+      </ButtonsGroupContainer>
     )
   }
 }
