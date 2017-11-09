ff04931e80e65dafa45ee5e05b9e4efa48f1da89
Tomasz Mittlener <t.mittlener@gmail.com>
2017-11-09 17:49:29 +0100

    feat: add button icon to for logout

--- src/components/shared/Header.react.js
+++ src/components/shared/Header.react.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 
 import styled from 'styled-components'
 import Button from 'react-bootstrap/lib/Button'
+import Glyphicon from 'react-bootstrap/lib/Glyphicon'
 
 const TopBar = styled.div`
   background-color: #222;
@@ -25,8 +26,9 @@ const TopBar = styled.div`
 const LoginForm = ({ onLogoutClick }) => (
   <TopBar>
     <h2 className="header-title">Welcome to SkyApp</h2>
+
     <Button className="logout-button" onClick={onLogoutClick}>
-      Log out
+      <Glyphicon glyph="log-out" />
     </Button>
   </TopBar>
 )
